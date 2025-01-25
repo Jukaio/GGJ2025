4329beb1e39d446508ed727b20e0d8e04cb4ac8a
David <D.Naussed@outlook.de>
2025-01-25 11:57:13 +0100

    Cleanup, Upgrade bubbles

--- src/main.cpp
+++ src/main.cpp
@@ -114,7 +114,7 @@ struct PlayerBubble
 		uint64_t archetype;
 		struct
 		{
-			uint8_t is_basic: 1;
+			uint8_t is_basic : 1;
 			uint8_t is_cat : 1;
 			uint8_t has_sun_glasses : 1;
 			uint8_t has_glasses : 1;
@@ -199,78 +199,6 @@ struct AutoBubble
 {
 	Bubble bubble;
 
-	// union
-	//{
-	//	uint64_t mask;
-	//	struct
-	//	{
-	//		uint8_t e00 : 1;
-	//		uint8_t e01 : 1;
-	//		uint8_t e02 : 1;
-	//		uint8_t e03 : 1;
-	//		uint8_t e04 : 1;
-	//		uint8_t e05 : 1;
-	//		uint8_t e06 : 1;
-	//		uint8_t e07 : 1;
-	//		uint8_t e08 : 1;
-	//		uint8_t e09 : 1;
-	//		uint8_t e10 : 1;
-	//		uint8_t e11 : 1;
-	//		uint8_t e12 : 1;
-	//		uint8_t e13 : 1;
-	//		uint8_t e14 : 1;
-	//		uint8_t e15 : 1;
-	//		uint8_t e16 : 1;
-	//		uint8_t e17 : 1;
-	//		uint8_t e18 : 1;
-	//		uint8_t e19 : 1;
-	//		uint8_t e20 : 1;
-	//		uint8_t e21 : 1;
-	//		uint8_t e22 : 1;
-	//		uint8_t e23 : 1;
-	//		uint8_t e24 : 1;
-	//		uint8_t e25 : 1;
-	//		uint8_t e26 : 1;
-	//		uint8_t e27 : 1;
-	//		uint8_t e28 : 1;
-	//		uint8_t e29 : 1;
-	//		uint8_t e30 : 1;
-	//		uint8_t e31 : 1;
-	//		uint8_t e32 : 1;
-	//		uint8_t e33 : 1;
-	//		uint8_t e34 : 1;
-	//		uint8_t e35 : 1;
-	//		uint8_t e36 : 1;
-	//		uint8_t e37 : 1;
-	//		uint8_t e38 : 1;
-	//		uint8_t e39 : 1;
-	//		uint8_t e40 : 1;
-	//		uint8_t e41 : 1;
-	//		uint8_t e42 : 1;
-	//		uint8_t e43 : 1;
-	//		uint8_t e44 : 1;
-	//		uint8_t e45 : 1;
-	//		uint8_t e46 : 1;
-	//		uint8_t e47 : 1;
-	//		uint8_t e48 : 1;
-	//		uint8_t e49 : 1;
-	//		uint8_t e50 : 1;
-	//		uint8_t e51 : 1;
-	//		uint8_t e52 : 1;
-	//		uint8_t e53 : 1;
-	//		uint8_t e54 : 1;
-	//		uint8_t e55 : 1;
-	//		uint8_t e56 : 1;
-	//		uint8_t e57 : 1;
-	//		uint8_t e58 : 1;
-	//		uint8_t e59 : 1;
-	//		uint8_t e60 : 1;
-	//		uint8_t e61 : 1;
-	//		uint8_t e62 : 1;
-	//		uint8_t e63 : 1;
-	//	};
-	// };
-
 	AutoBubbleIncremental inc;
 
 	SDL_Color color;
@@ -279,6 +207,30 @@ struct AutoBubble
 	float y;
 	float width;
 	float height;
+};
+
+
+struct UpgradeBubbleIncremental
+{
+	// Config
+	uint64_t cost;
+
+	uint64_t base_bonus;
+	uint64_t multiplier_bonus;
+};
+
+struct UpgradeBubble
+{
+	Bubble bubble;
+
+	UpgradeBubbleIncremental inc;
+
+	float x;
+	float y;
+	float width;
+	float height;
+
+	SDL_Color color;
 };
 
 float lerp(float a, float b, float t) { return a + (b - a) * t; }
@@ -489,6 +441,47 @@ void update(App* app, SinglePlayer* player, AutoBubble* bubbles, size_t count)
 	}
 }
 
+void update(App* app, SinglePlayer* player, UpgradeBubble* bubbles, size_t count)
+{
+	bool is_player_clicking = button_just_down(&app->input.mouse, 1);
+
+	for (size_t index = 0; index < count; index++)
+	{
+		UpgradeBubble* bubble = &bubbles[index];
+
+		MouseState const* state = &app->input.mouse.current;
+		float global_x = bubble->bubble.x + bubble->x;
+		float global_y = bubble->bubble.y + bubble->y;
+		float distance = math_distance(state->x, state->y, global_x, global_y);
+		if (player->current_money < bubble->inc.cost)
+		{
+			bubble->bubble.color = SDL_Color{ 128, 128, 128, 255 };
+			continue;
+		}
+
+		if (distance < bubble->bubble.radius)
+		{
+			bubble->bubble.color = SDL_Color{ 255, 0, 0, 255 };
+
+			if (is_player_clicking)
+			{
+				player->current_money = player->current_money - bubble->inc.cost;
+				player->base = player->base + bubble->inc.base_bonus;
+				player->multiplier = player->multiplier + bubble->inc.multiplier_bonus;
+
+				bubble->bubble.time_since_clicked = app->now;
+				bubble->bubble.color = SDL_Color{ 255, 255, 255, 255 };
+			}
+		}
+		else
+		{
+			bubble->bubble.color = SDL_Color{ 255, 0, 255, 255 };
+		}
+	}
+}
+
+
+
 void fixed_update(App* app, SinglePlayer* player, AutoBubble* bubbles, size_t count)
 {
 	bool is_player_clicking = button_just_down(&app->input.mouse, 1);
@@ -562,6 +555,40 @@ void render(App* app, AutoBubble* bubbles, size_t count)
 	}
 }
 
+void render(App* app, UpgradeBubble* bubbles, size_t count)
+{
+	for (size_t index = 0; index < count; index++)
+	{
+		const UpgradeBubble* auto_bubble = &bubbles[index];
+		const Bubble* bubble = &auto_bubble->bubble;
+
+		{
+			SDL_Color c = auto_bubble->color;
+			SDL_SetRenderDrawColor(app->renderer, c.r, c.g, c.b, c.a);
+
+			SDL_FRect dst = SDL_FRect{ auto_bubble->x, auto_bubble->y, auto_bubble->width, auto_bubble->height };
+			SDL_RenderRect(app->renderer, &dst);
+		}
+
+		{
+			SDL_Color c = bubble->color;
+
+			SDL_Texture* texture = tex[(uint64_t)Sprite::BubbleGhost];
+			float w, h;
+			SDL_GetTextureSize(texture, &w, &h);
+			SDL_FRect src = SDL_FRect{ 0, 0, w, h };
+
+			SDL_SetTextureColorMod(texture, c.r, c.g, c.b);
+
+			SDL_FRect dst = get_frect(app, bubble);
+			dst.x = dst.x + auto_bubble->x;
+			dst.y = dst.y + auto_bubble->y;
+			SDL_RenderTexture(app->renderer, texture, &src, &dst);
+		}
+	}
+}
+
+
 void render(App* app, SinglePlayer* player)
 {
 	if (player->previous_money != player->current_money)
@@ -570,50 +597,20 @@ void render(App* app, SinglePlayer* player)
 	}
 }
 
-int main(int argc, char* argv[])
+
+constexpr int window_width = 1920;
+constexpr int window_height = 1080;
+
+constexpr int window_width_half = window_width / 2;
+constexpr int window_height_half = window_height / 2;
+
+constexpr float bubble_click_duration = 0.25f;
+constexpr float bubble_click_scale = 0.60f;
+
+void setup(PlayerBubble* player_bubbles, size_t count)
 {
-	platform_init();
-	SDL_Init(SDL_INIT_VIDEO);
+	SDL_assert(count == 1 && "We only handle one player bubble for now");
 
-	if (!TTF_Init())
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not init TTF: %s\n", SDL_GetError());
-		return -1;
-	}
-
-	constexpr int window_width = 1920;
-	constexpr int window_height = 1080;
-
-	constexpr int window_width_half = window_width / 2;
-	constexpr int window_height_half = window_height / 2;
-
-	App app{};
-	app.tick_frequency = 1.0f;
-	app.window = SDL_CreateWindow("Bubble Clicker", window_width, window_height, 0);
-	if (app.window == nullptr)
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-		return -1;
-	}
-	app.renderer = SDL_CreateRenderer(app.window, nullptr);
-	if (app.renderer == nullptr)
-	{
-		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-		return -1;
-	}
-
-	load_assets(app.renderer);
-
-	SinglePlayer player{};
-	player.base = 1;
-	player.multiplier = 1;
-
-	constexpr float bubble_click_duration = 0.25f;
-	constexpr float bubble_click_scale = 0.60f;
-
-	// Only one bubble for now
-	constexpr size_t player_bubble_count = 1;
-	PlayerBubble player_bubbles[player_bubble_count];
 	player_bubbles->bubble.x = window_width_half;
 	player_bubbles->bubble.y = window_height_half;
 	player_bubbles->bubble.radius = 256.0f;
@@ -622,16 +619,16 @@ int main(int argc, char* argv[])
 	player_bubbles->bubble.duration_click = bubble_click_duration;
 
 	print(player_bubbles);
+}
 
-	constexpr size_t auto_bubble_count = 9;
-	AutoBubble auto_bubbles[auto_bubble_count]{};
-
+void setup(AutoBubble* auto_bubbles, size_t auto_bubble_count)
+{
 	constexpr float radius = 48.0f;
 	constexpr float width = 128.0f;
 	constexpr float height = 96.0f;
 	constexpr float origin_x = 32.0f;
 	constexpr float origin_y = 32.0f;
-	constexpr float offset_y = 128.0f;
+	constexpr float offset_y = 112.0f;
 	for (size_t index = 0; index < auto_bubble_count; index++)
 	{
 		AutoBubble* auto_bubble = &auto_bubbles[index];
@@ -662,6 +659,92 @@ int main(int argc, char* argv[])
 		print(auto_bubble);
 		SDL_Log("");
 	}
+}
+
+void setup(UpgradeBubble* upgrade_bubbles, size_t auto_bubble_count)
+{
+	constexpr float radius = 48.0f;
+	constexpr float width = 128.0f;
+	constexpr float height = 96.0f;
+	constexpr float origin_x = window_width - (32.0f + width);
+	constexpr float origin_y = 32.0f;
+	constexpr float offset_y = 112.0f;
+	for (size_t index = 0; index < auto_bubble_count; index++)
+	{
+		UpgradeBubble* upgrade_bubble = &upgrade_bubbles[index];
+		Bubble* bubble = &upgrade_bubble->bubble;
+
+		upgrade_bubble->x = origin_x;
+		upgrade_bubble->y = origin_y + (index * offset_y);
+
+		upgrade_bubble->width = width;
+		upgrade_bubble->height = height;
+
+		upgrade_bubble->color = SDL_Color{ 255, 255, 0, 255 };
+
+		bubble->x = radius;
+		bubble->y = radius;
+		bubble->radius = radius;
+		bubble->color = SDL_Color{ 255, 0, 255, 255 };
+
+		UpgradeBubbleIncremental config = {};
+		config.cost = (32 + (index * 2) * 32);
+		config.base_bonus = 1 + (index * 2);
+		config.multiplier_bonus = 1 + (index * 2);
+
+		bubble->click_scale = bubble_click_scale;
+		bubble->duration_click = bubble_click_duration;
+
+		SDL_Log("");
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	platform_init();
+	SDL_Init(SDL_INIT_VIDEO);
+
+	if (!TTF_Init())
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not init TTF: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	App app{};
+	app.tick_frequency = 1.0f;
+	app.window = SDL_CreateWindow("Bubble Clicker", window_width, window_height, 0);
+	if (app.window == nullptr)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+		return -1;
+	}
+	app.renderer = SDL_CreateRenderer(app.window, nullptr);
+	if (app.renderer == nullptr)
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+		return -1;
+	}
+
+	load_assets(app.renderer);
+
+	SinglePlayer player{};
+	player.base = 1;
+	player.multiplier = 1;
+
+	// Only one bubble for now
+	constexpr size_t player_bubble_count = 1;
+	PlayerBubble player_bubbles[player_bubble_count];
+
+	constexpr size_t auto_bubble_count = 9;
+	AutoBubble auto_bubbles[auto_bubble_count]{};
+
+	constexpr size_t upgrade_bubble_count = 4;
+	UpgradeBubble upgrade_bubbles[upgrade_bubble_count]{};
+
+	setup(player_bubbles, player_bubble_count);
+	setup(auto_bubbles, auto_bubble_count);
+	setup(upgrade_bubbles, upgrade_bubble_count);
 
 	bool is_running = true;
 
@@ -699,14 +782,17 @@ int main(int argc, char* argv[])
 		// Update
 		update(&app, &player, player_bubbles, player_bubble_count);
 		update(&app, &player, auto_bubbles, auto_bubble_count);
+		update(&app, &player, upgrade_bubbles, upgrade_bubble_count);
 
 		// Render
 		SDL_SetRenderDrawColor(app.renderer, 0, 0, 0, 0);
 
 		SDL_RenderClear(app.renderer);
 
-		render(&app, player_bubbles, player_bubble_count);
 		render(&app, auto_bubbles, auto_bubble_count);
+		render(&app, upgrade_bubbles, upgrade_bubble_count);
+		render(&app, player_bubbles, player_bubble_count);
+
 		render(&app, &player);
 
 		SDL_RenderPresent(app.renderer);
